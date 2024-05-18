ecd94d6f790b1b89a9640a0dc7fcf0b137672bca
Rifyal05 <rifyalaidil05@gmail.com>
2024-05-18 22:55:48 +0700

    test

--- exer.py
+++ exer.py
@@ -1,6 +1,7 @@
 # Import exer_modul sebagai modul 'exorcist'
 import exer_modul as exorcist
 
+
 # Looping atau perulangan untuk menjalankan pogram berulang kali
 while True:
         # Daftar pilihan yang dapat di jalankan oleh program ini
